2fef6a189dc3599465d060299a153df8eb0a0ef9
Mohammad413 <69928690+Mohammad413@users.noreply.github.com>
2022-10-29 12:12:35 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { Forms } from "./App";
+import "./style.css";
+
+ReactDom.render(<Forms />, document.getElementById("container"));
