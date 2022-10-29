c146f860760335975b95583831a22e3cb7e31943
Mohammad413 <69928690+Mohammad413@users.noreply.github.com>
2022-10-29 12:13:02 -0400

    Create people.js

---
+++ people.js
@@ -0,0 +1,18 @@
+let People = [
+  {
+    name: "Ronaldo",
+    image:
+      "https://media.newyorker.com/photos/61e87281b67066a13fd20ea8/3:4/w_1278,h_1704,c_limit/Caesar-Ronaldo.jpg"
+  },
+  {
+    name: "Messi",
+    image:
+      "https://cdn.britannica.com/34/212134-050-A7289400/Lionel-Messi-2018.jpg"
+  },
+  {
+    name: "Neymar",
+    image:
+      "https://nationaltoday.com/wp-content/uploads/2022/03/Neymar-Birthday.jpg.webp"
+  }
+];
+export { People };
